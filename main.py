d5e9e1645488dca73a304e3457edb408b1ac46b5
Arnav Tripathi <arnavtripathi@Arnavs-MacBook-Air.local>
2024-05-12 01:33:56 +0530

    Adding PostGreUrl

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ gravatar = Gravatar(app,
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI', 'sqlite:///posts.db')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
